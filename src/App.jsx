485bc7f51e6f541ce88ec7eca74155603f826d6e
Elibabah <delias16.hernandez@gmail.com>
2022-03-15 15:24:07 -0600

    first grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import './App.css';
 import NavBar from "./components/NavBar"
 import { NavBarBoot } from "./components/NavBarBootstrap"
+import Footer from "./components/Footer"
+import Projects from "./components/Projects"
+import { Contact } from "./components/Contact" 
 import About from "./components/About"
 
 function App() {
   return (
-    <div className="App">
-      <NavBarBoot/>
-      <About/>
-    </div>
+    <main className='App'>
+
+      <div className="navBar">
+        <NavBarBoot/>
+        <About/>
+      </div>
+
+      <div className="projects">
+        <Projects/>
+      </div>
+
+      <div className="contact">
+        <Contact/>
+      </div>
+
+      <div className="footer">
+        <Footer/>
+      </div>
+
+    </main>
   );
 }
 
