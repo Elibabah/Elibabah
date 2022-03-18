eedcd8c66331223a85fe4fcd8e6d5fc0634cd4e4
Elibabah <delias16.hernandez@gmail.com>
2022-03-17 21:02:06 -0600

    eye

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import "./Home.css"
 
 export const Home = () => {
+
+  let ojo = document.querySelector(".ojo")
+  let punto = document.querySelector(".punto")
+
+  document.onmousemove = (e) => {
+    let x = e.clientX * 100 / window.innerWidth + "%"
+  
+    let y = e.clientY * 100 / window.innerHeight + "%"
+
+    punto.style.left = x;
+    punto.style.top = y;
+  }
+
   return (
-    <div>Home</div>
+    <div>
+      <div className='ojo'>
+        <div className='punto'></div>
+      </div>
+    </div>
   )
 }
